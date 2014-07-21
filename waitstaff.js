ec18213fdac5535f01a1e93bd270b41f861fc173
bdtomlin <bdtomlin@gmail.com>
2014-07-20 19:02:14 -0500

    finished adding routes to waitstaff calculator

--- waitstaff.js
+++ waitstaff.js
@@ -1,70 +1,87 @@
-'use strict';
-
-angular.module('waitstaff', [])
-  .controller('MainCtrl', ['$scope', function($scope){
-    var initializeData = function(){
-      $scope.data = {
-        customerSubtotal: 0,
-        customerTip: 0,
-        customerTotal: 0,
-        tipHistory: []
-      };
-    };
-    initializeData();
-
-    var customerTotal = function(customerSubtotal, customerTip){
-      return customerSubtotal + customerTip;
-    };
-
-    var customerSubtotal = function(baseMealPrice, taxRate){
-      var subTot = parseFloat(baseMealPrice);
-      subTot = subTot + subTot * taxRate / 100; 
-      return subTot;
-    };
-
-    var calculateTip = function(customerSubtotal, tipPercent){
-      return tipPercent / 100 * customerSubtotal;
-    }
-
-    $scope.myTipTotal = function(){
-      var total = 0;
-      for(var i=0, l=$scope.data.tipHistory.length; i<l; i++){
-        total += $scope.data.tipHistory[i];
+angular.module('waitstaff', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      templateUrl: 'home.html',
+      controller: 'HomeCtrl'
+    }).when('/new-meal',{
+      templateUrl: 'new-meal.html',
+      controller: 'NewMealCtrl'
+    }).when('/my-earnings',{
+      templateUrl: 'my-earnings.html',
+      controller: 'MyEarningsCtrl'
+    });
+  })
+  .factory('Data', function(){
+    return {
+      customerSubtotal: 0,
+      customerTip: 0,
+      customerTotal: 0,
+      tipHistory: [],
+      clearInputs: function(){
+        this.baseMealPrice = null;
+        this.taxRate = null;
+        this.tipPercent = null;
+      },
+      reset: function(){
+        this.customerSubtotal = 0
+        this.customerTip = 0
+        this.customerTotal = 0
+        this.tipHistory = []
+      },
+      myTipTotal: function(){
+        var total = 0;
+        for(var i=0, l=this.tipHistory.length; i<l; i++){
+          total += this.tipHistory[i];
+        }
+        return total;
+      },
+      avgTip: function(){
+        if(this.tipHistory.length){
+          return this.myTipTotal() / this.tipHistory.length;
+        }else{
+          return 0;
+        }
+      },
+      calculateCustomerSubtotal: function(){
+        var subTot = parseFloat(this.baseMealPrice);
+        subTot = subTot + subTot * this.taxRate / 100; 
+        return subTot;
+      },
+      calculateCustomerTotal: function(){
+        return this.customerSubtotal + this.customerTip;
+      },
+      calculateTip: function(){
+        return this.tipPercent / 100 * this.customerSubtotal;
+      },
+      createNewMeal: function(){
+        this.customerSubtotal = this.calculateCustomerSubtotal();
+        this.customerTip = this.calculateTip();
+        this.customerTotal = this.calculateCustomerTotal();
+        this.tipHistory.push(this.customerTip)
       }
-      return total;
     };
-
-    $scope.avgTip = function(){
-      if($scope.data.tipHistory.length){
-        return $scope.myTipTotal() / $scope.data.tipHistory.length;
-      }else{
-        return 0;
-      }
-    };
+  })
+  .controller('HomeCtrl', function($scope){
+  })
+  .controller('NewMealCtrl', function($scope, Data){
+    $scope.data = Data;
 
     $scope.wsFormSubmit = function(){
       $scope.submitted = true;
       if($scope.inputForm.$valid){
-        $scope.data.customerSubtotal = customerSubtotal($scope.data.baseMealPrice, $scope.data.taxRate);
-        $scope.data.customerTip = calculateTip($scope.data.customerSubtotal, $scope.data.tipPercent);
-        $scope.data.customerTotal = customerTotal($scope.data.customerSubtotal, $scope.data.customerTip);
-        $scope.data.tipHistory.push($scope.data.customerTip)
+        $scope.data.createNewMeal();
       }
     };
 
-    var clearForm = function(){
-      $scope.submitted = false;
-      $scope.data.baseMealPrice = null;
-      $scope.data.taxRate = null;
-      $scope.data.tipPercent = null;
-    };
-
     $scope.cancel = function(){
-      clearForm();
+      $scope.submitted = false;
+      $scope.data.clearInputs();
     };
-
+  })
+  .controller('MyEarningsCtrl', function($scope, Data){
+    $scope.data = Data;
     $scope.reset = function(){
-      clearForm();
-      initializeData();
+      $scope.data.clearInputs();
+      $scope.data.reset();
     };
-  }]);
+  });
